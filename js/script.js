2190e212ca2f9cbe6f65bf36917be35e38c3812a
kastastin <kastastin.main@gmail.com>
2024-10-19 20:16:10 +0300

    add tabs section (1920px)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+const tabsBtns = document.querySelectorAll(".tabs__btns-item");
+const tabsContentItems = document.querySelectorAll(".tabs__content-item");
+
+tabsBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const tabTarget = e.currentTarget;
+    const activeContent = tabTarget.dataset.btn; // data-btn="content-1"
+
+    tabsBtns.forEach((item) => {
+      item.classList.remove("tabs__btns-item_active");
+    });
+
+    tabTarget.classList.add("tabs__btns-item_active");
+
+    tabsContentItems.forEach((content) => {
+      content.classList.remove("tabs__content-item_active");
+    });
+
+    document
+      .querySelector(`#${activeContent}`)
+      .classList.add("tabs__content-item_active");
+  });
+});
+
 const swiper = new Swiper(".swiper", {
   loop: true,
   effect: "fade",
